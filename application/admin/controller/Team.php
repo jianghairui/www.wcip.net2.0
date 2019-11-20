b5db02606259411ebe98c242edeb70862c8177b5
Jiang <1873645345@qq.com>
2019-11-20 18:19:02 +0800

    H5重写50%

---
+++ application/admin/controller/Team.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jiang
+ * Date: 2019/11/20
+ * Time: 11:11
+ */
+namespace app\admin\controller;
+
+use think\Db;
+class Team extends Base {
+
+    public function teamList() {
+
+    }
+
+    public function teamAdd() {
+        return $this->fetch();
+    }
+
+    public function teamDetail() {
+
+    }
+
+    public function teamMod() {
+
+    }
+
+    public function teamHide() {
+
+    }
+
+    public function teamShow() {
+
+    }
+
+    public function teamDel() {
+
+    }
+
+
+
+
+
+}
