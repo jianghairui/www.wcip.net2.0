b1a49fe9c44c91d99e682f6053262c44bfdcebce
Jiang <1873645345@qq.com>
2019-11-21 13:08:12 +0800

    H5重写50%

--- application/admin/controller/Team.php
+++ application/admin/controller/Team.php
@@ -11,31 +11,169 @@ use think\Db;
 class Team extends Base {
 
     public function teamList() {
+        $param['search'] = input('param.search');
+        $page['query'] = http_build_query(input('param.'));
 
+        $curr_page = input('param.page',1);
+        $perpage = input('param.perpage',10);
+        $where = [];
+        if($param['search']) {
+            $where[] = ['title','like',"%{$param['search']}%"];
+        }
+
+        $count = Db::table('mp_team')->where($where)->count();
+
+        $page['count'] = $count;
+        $page['curr'] = $curr_page;
+        $page['totalPage'] = ceil($count/$perpage);
+        try {
+            $list = Db::table('mp_team')
+                ->where($where)
+                ->order(['sort'=>'ASC','create_time'=>'DESC'])
+                ->limit(($curr_page - 1)*$perpage,$perpage)->select();
+        }catch (\Exception $e) {
+            die('SQL错误: ' . $e->getMessage());
+        }
+        $this->assign('list',$list);
+        $this->assign('page',$page);
+        $this->assign('param',$param);
+        return $this->fetch();
     }
 
     public function teamAdd() {
         return $this->fetch();
     }
 
-    public function teamDetail() {
+    public function teamAddPost() {
+        $val['name'] = input('post.name');
+        $val['title'] = input('post.title');
+        $val['desc'] = input('post.desc');
+        $val['status'] = input('post.status');
+        checkInput($val);
+        if(isset($_FILES['file'])) {
+            $info = upload('file',$this->upload_base_path);
+            if($info['error'] === 0) {
+                $val['pic'] = $info['data'];
+            }else {
+                return ajax($info['msg'],-1);
+            }
+        }else {
+            return ajax('请上传封面图',-1);
+        }
+        try {
+            Db::table('mp_team')->insert($val);
+        }catch (\Exception $e) {
+            if(isset($val['pic'])) {
+                @unlink($val['pic']);
+            }
+            return ajax($e->getMessage(),-1);
+        }
+        return ajax([]);
+    }
 
+    public function teamDetail() {
+        $team_id = input('param.id');
+        try {
+            $exist = Db::table('mp_team')->where('id','=',$team_id)->find();
+            if(!$exist) {
+                die('非法操作');
+            }
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+        $this->assign('info',$exist);
+        return $this->fetch();
     }
 
     public function teamMod() {
-
+        $val['name'] = input('post.name');
+        $val['title'] = input('post.title');
+        $val['desc'] = input('post.desc');
+        $val['id'] = input('post.id');
+        $val['status'] = input('post.status');
+        checkInput($val);
+        if(isset($_FILES['file'])) {
+            $info = upload('file',$this->upload_base_path);
+            if($info['error'] === 0) {
+                $val['pic'] = $info['data'];
+            }else {
+                return ajax($info['msg'],-1);
+            }
+        }
+        $where = [
+            ['id','=',$val['id']]
+        ];
+        try {
+            $exist = Db::table('mp_team')->where($where)->find();
+            if(!$exist) {
+                return ajax('非法参数',-1);
+            }
+            Db::table('mp_team')->where($where)->update($val);
+        }catch (\Exception $e) {
+            if(isset($val['pic'])) {
+                @unlink($val['pic']);
+            }
+            return ajax($e->getMessage(),-1);
+        }
+        if(isset($val['pic'])) {
+            @unlink($exist['pic']);
+        }
+        return ajax([]);
     }
 
     public function teamHide() {
-
+        $val['id'] = input('post.id');
+        checkInput($val);
+        $where = [
+            ['id','=',$val['id']]
+        ];
+        try {
+            $exist = Db::table('mp_team')->where($where)->find();
+            if (!$exist) {
+                return ajax('非法操作', -1);
+            }
+            Db::table('mp_team')->where($where)->update(['status' => 0]);
+        } catch (\Exception $e) {
+            return ajax($e->getMessage(), -1);
+        }
+        return ajax();
     }
 
     public function teamShow() {
-
+        $val['id'] = input('post.id');
+        checkInput($val);
+        $where = [
+            ['id','=',$val['id']]
+        ];
+        try {
+            $exist = Db::table('mp_team')->where($where)->find();
+            if(!$exist) {
+                return ajax('非法操作',-1);
+            }
+            Db::table('mp_team')->where($where)->update(['status'=>1]);
+        } catch (\Exception $e) {
+            return ajax($e->getMessage(), -1);
+        }
+        return ajax();
     }
 
     public function teamDel() {
-
+        $val['id'] = input('post.id');
+        checkInput($val);
+        $where = [
+            ['id','=',$val['id']]
+        ];
+        try {
+            $exist = Db::table('mp_team')->where($where)->find();
+            if(!$exist) {
+                return ajax('非法操作',-1);
+            }
+            Db::table('mp_team')->where($where)->delete();
+        }catch (\Exception $e) {
+            return ajax($e->getMessage(),-1);
+        }
+        @unlink($exist['pic']);
+        return ajax([],1);
     }
 
 
